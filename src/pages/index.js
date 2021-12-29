82cb28eca8bea33c4c7adebc27fc222331a4150e
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-12-29 15:04:43 +0530

    filename error fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,6 @@ import HomePage from './HomePage';
 import ContactPage from './ContactPage';
 import ProjectsPage from './ProjectsPage';
 import ErrorPage from './ErrorPage';
-import Links from './Links';
+import Links from './links';
 
 export { HomePage, ContactPage, ProjectsPage, ErrorPage, Links };
