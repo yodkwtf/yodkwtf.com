0df88fb3668abd29cc5f79f1b9d67f06d3e3ee3e
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2022-08-14 03:28:36 +0530

    logo warning removed

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../images/logo.png';
+// import logo from '../images/logo.png';
 import logoSVG from '../images/logo.svg';
 
 const Logo = () => {
